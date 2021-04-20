adde9fb1e32b473407bbe902887589f3c986caaf
kenisha-star <77574261+kenisha-star@users.noreply.github.com>
2021-04-20 17:36:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function para() {
+  var array1 = [];
+  for (i = 1; i <= 5; i++) {
+    array1.push(document.getElementById("w" + i).value);
+  }
+  document.getElementById("div1").innerHTML = array1.join();
+}
