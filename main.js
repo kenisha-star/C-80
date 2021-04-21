86f329007c96fa360f4c9bbd678e9712be13266b
kenisha-star <77574261+kenisha-star@users.noreply.github.com>
2021-04-21 11:01:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function para() {
   var array1 = [];
-  for (i = 1; i <= 5; i++) {
+  for (i = 1; i <= 8; i++) {
     array1.push(document.getElementById("w" + i).value);
   }
   document.getElementById("div1").innerHTML = array1.join();
